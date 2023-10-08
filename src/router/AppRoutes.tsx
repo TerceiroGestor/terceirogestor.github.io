5110d67e13eebd4b0a2eab27cf94fa8e4459ad94
Weverton Campos <79879949+wevertoncamposdev@users.noreply.github.com>
2023-10-08 15:55:34 -0300

    update

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //Page
-import Navbar from '../components/Navbar';
+//import Navbar from '../components/Navbar';
 import Home from '../components/Home';
 import About from '../components/About';
 import Team from '../components/Team';
@@ -9,10 +9,9 @@ import Team from '../components/Team';
 const AppRoutes = () => {
     return (
         <Router>
-            <Navbar />
             <Routes>
-                <Route path="/" Component={Home} />
-                <Route path="/about" Component={About} />
+                <Route path="/" Component={About} />
+                <Route path="/home" Component={Home} />
                 <Route path="/team" Component={Team} />
             </Routes>
         </Router>
